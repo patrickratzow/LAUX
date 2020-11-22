d26455ad1d73f7ce6c404ac1ff6ed16d0122c0f1
Patrick Ratzow <patrickratzow@hotmail.dk>
2020-11-22 16:59:11 +0100

    improv(tests): check true/false, not equals

--- test/parser/gettersetter.test.js
+++ test/parser/gettersetter.test.js
@@ -19,21 +19,21 @@ export default {
       var ast = parser.parse(code);
       var statement = ast.chunk.body[0].body[0];
 
-      expect(statement.isGet).to.equal(true)
-      expect(statement.isSet).to.equal(false)
+      expect(statement.isGet).to.be.true;
+      expect(statement.isSet).to.be.false;
   },
   onlyHasSet: function() {
     var ast = parser.parse("public class Test _set id end");
     var statement = ast.chunk.body[0].body[0];
 
-    expect(statement.isGet).to.equal(false)
-    expect(statement.isSet).to.equal(true)
+    expect(statement.isGet).to.be.false;
+    expect(statement.isSet).to.be.true;
   },
   hasSetAndGet: function() {
     var ast = parser.parse("public class Test _get _set id end");
     var statement = ast.chunk.body[0].body[0];
 
-    expect(statement.isGet).to.equal(true)
-    expect(statement.isSet).to.equal(true)
+    expect(statement.isGet).to.be.true;
+    expect(statement.isSet).to.be.true;
   }
 }
